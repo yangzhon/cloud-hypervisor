7f2be11971fdb34ab30599d2627ddf85db9ca740
Yang Zhong <yang.zhong@intel.com>
2019-08-12 17:48:19 +0800

    vhost-user-blk: enable write_config for WCE
    In vhost-user-blk, only WCE value can be set back to device in
    guest kernel like
    echo "write through" > /sys/block/vda/cache_type
    
    So write_config() will only set WCE value from guest kernel to
    vhost user side.
    
    Signed-off-by: Yang Zhong <yang.zhong@intel.com>

--- vm-virtio/src/vhost_user/blk.rs
+++ vm-virtio/src/vhost_user/blk.rs
@@ -28,6 +28,8 @@ use std::mem;
 
 pub const VIRTIO_F_VERSION_1_BITMASK: u64 = 1 << VIRTIO_F_VERSION_1;
 pub const VIRTIO_F_VERSION_1: ::std::os::raw::c_uint = 32;
+pub const VIRTIO_BLK_F_CONFIG_WCE: ::std::os::raw::c_uint = 11;
+pub const VIRTIO_BLK_F_CONFIG_WCE_BITMASK: u64 = 1 << VIRTIO_BLK_F_CONFIG_WCE;
 
 macro_rules! offset_of {
     ($ty:ty, $field:ident) => {
@@ -114,6 +116,10 @@ impl Blk {
         // Only set wce value(u8).
         config_space[wce_offset] = config_wce;
 
+        if config_wce ==1 {
+            // SPDK should expose WCE feature by default.
+            avail_features |= VIRTIO_BLK_F_CONFIG_WCE_BITMASK;
+        }
         let queue_num_offset = offset_of!(virtio_blk_config, num_queues);
 
         // Only set num_queues value(u16).
@@ -300,8 +306,17 @@ impl VirtioDevice for Blk {
             error!("Failed to write config space");
             return;
         }
-        let (_, right) = self.config_space.split_at_mut(offset as usize);
-        right.copy_from_slice(&data[..]);
+        // In fact, write_config() only handle wce value in vhost-user-blk.
+        // so, we can only set wce value here.
+        // Since vhost_set_config callback function in SPDK is NULL, which show
+        // there is not any handle in SPDK side for set_config operation. The
+        // detailed info ref spdk_extern_vhost_pre_msg_handler() in SPDK.
+        if self.config_space[offset as usize] == data[0] {
+            return;
+        }
+        self.vhost_user_blk
+            .set_config(offset as u32, data, VhostUserConfigFlags::WRITABLE);
+        self.config_space[offset as usize] = data[0];
     }
 
     fn activate(
